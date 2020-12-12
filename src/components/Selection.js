7b454c8c6f817f9cfeea460328192fceedfc4c9b
Hruday Bharadwaj <saihruday.08@gmail.com>
2020-12-12 18:01:59 +0530

    chngs

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,13 @@
+import React, { useState } from "react";
 
+export default function Selection(props) {
+  let color = props.applyColor;
+
+  let [style, setStyle] = useState({ background: "" });
+
+  return (
+    <div className="fix-box" onClick={() => color()}>
+      <h2 className="subheading">Selection</h2>
+    </div>
+  );
+}
