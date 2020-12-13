71b37731f7a3464e38daef5f278d2dd1ea532780
Hruday Bharadwaj <saihruday.08@gmail.com>
2020-12-13 23:05:28 +0530

    chngs

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -4,10 +4,9 @@ export default function Selection(props) {
   let [style, setStyle] = useState({ background: "" });
 
   let applyColor = props.applyColor;
-  let background = props.nextBackground;
 
   return (
-    <div className="fix-box" onClick={() => setStyle(applyColor)} style={style}>
+    <div className="fix-box" onClick={applyColor} style={style}>
       <h2 className="subheading">Selection</h2>
     </div>
   );
